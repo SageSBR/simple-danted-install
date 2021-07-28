a00e1e865cc08e6fca1af07c782ce043ecb14c99
Saulo Germano <sgwebr@gmail.com>
2021-07-28 16:49:31 -0300

    Update danteinstall.sh

--- danteinstall.sh
+++ danteinstall.sh
@@ -9,10 +9,10 @@ if [ "$#" -ne 1 ]; then
 	exit 1
 fi
 
-sudo apt update
+apt update
 wget http://archive.ubuntu.com/ubuntu/pool/universe/d/dante/dante-server_1.4.2+dfsg-7build3.1_amd64.deb
-sudo dpkg -i dante-server_*.deb
-sudo useradd --shell /usr/sbin/nologin proxyuser
+dpkg -i dante-server_*.deb
+useradd --shell /usr/sbin/nologin proxyuser
 echo "proxyuser:$password" | chpasswd
 
 echo "Copy config to /etc/danted.conf ..."
@@ -31,10 +31,10 @@ client pass {
 socks pass {
         from: 0/0 to: 0/0
         log: connect disconnect error
-}" | sudo tee /etc/danted.conf
+}" | tee /etc/danted.conf
 
-sudo systemctl restart danted
-sudo systemctl enable danted
-sudo systemctl status danted
+systemctl restart danted
+systemctl enable danted
+systemctl status danted
 
 echo "Install complete.\nYou can use proxyuser and the supplied password to log into the proxy!"
